9b9796cf5291651bf8cc158811b99bcb50192cfa
Bogdan Demchenko <bogdandemchenko@gmail.com>
2017-06-07 14:21:56 +0300

    news item detail

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -13,13 +13,34 @@ class NewsController extends Controller
 		echo "this is my first controller";
 	}
 
-	function actionItemsList()
+	function dataItems()
 	{
 		$newsList = [
 			['title' => 'First World War', 'date' => '1914-07-28'],
 			['title' => 'Second World War', 'date' => '1939-09-01'],
-			['title' => 'First man on the moon', 'date' => '1969-07-20']
+			['title' => 'First man on the moon', 'date' => '1969-07-20'],
+			['title' => 'Test', 'date' => '1969-07-20']
 		];
+
+		return $newsList;
+	}
+
+	function actionItemsList()
+	{
+		$newsList = $this->dataItems();
+
 		return $this->render('itemsList', ['newsList' => $newsList]);
+	}
+
+	function actionItemDetail($title)
+	{
+		$newsList = $this->dataItems();
+
+		$item = null;
+		foreach ($newsList as $n) {
+			if ($title == $n['title']) $item = $n;
+		}
+
+		return $this->render('itemDetail', ['item' => $item]);
 	}
 }
