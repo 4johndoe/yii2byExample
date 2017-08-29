a0a0478344d1012357ee5f2ea08d07e5435732c7
4johndoe <bogdandemchenko@gmail.com>
2017-08-29 10:33:13 +0000

    apply new db setting

--- config/db.php
+++ config/db.php
@@ -2,8 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=crmapp',
+    // 'dsn' => 'sqlite:'.dirname(__DIR__).'/db.sqlite',
     'username' => 'root',
-    'password' => '',
+    'password' => 'smap',
     'charset' => 'utf8',
 ];
